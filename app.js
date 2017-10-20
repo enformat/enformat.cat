22acee11caef671f73831461be5c66c4585c555f
Enric Forn <enricfj@gmail.com>
2017-10-20 08:30:55 +0200

    Add reCAPTCHA

--- app.js
+++ app.js
@@ -37,28 +37,59 @@ app.use('/privacy-policy', routes);
 app.use('/development-operations-qa', routes)
 app.use('/containerize-your-platform',routes)
 
-app.post('/index', function(req, res){
+app.post('/contact', function(req, res){
    
     console.dir(req.body);
     
     if (req.body.action_contact == 1){
-           
-      var msg = 'Name :<b>' + req.body.Name+'</b><br />'+
-                'Email :<b>' + req.body.Email+'</b><br />'+
-                'Phone :<b>' + req.body.Telephone+'</b><br />'+
-                'Message :<b>' + req.body.Message +'</b>';
-        
-      sendmail({
-        from: "no-reply@enformat.cat", // sender address
-    	to: 'info@enformat.cat', // list of receivers
-    	subject: "Contact from website", // Subject line
-    	html: msg // html body
-      }, function(err, reply) {
-        console.log(err && err.stack);
-        console.dir(reply);
-      });
+      verifyRecaptcha(req.body["g-recaptcha-response"], function(success) {
+                if (success) {
+                  //recaptcha OK
+                  var msg = 'Name :<b>' + req.body.Name+'</b><br />'+
+                  'Email :<b>' + req.body.Email+'</b><br />'+
+                  'Phone :<b>' + req.body.Telephone+'</b><br />'+
+                  'Message :<b>' + req.body.Message +'</b>';
+          
+                  sendmail({
+                    from: "no-reply@enformat.cat", // sender address
+                  to: 'info@enformat.cat', // list of receivers
+                  subject: "Contact from website", // Subject line
+                  html: msg // html body
+                  }, function(err, reply) {
+                    console.log(err && err.stack);
+                    console.dir(reply);
+                  });
+                } 
+                else 
+                {
+                  //recaptcha KO
+                  res.redirect(401, 'back');
+                }
+        });
    }
+
+   res.redirect(200, 'back');
 });
+
+var SECRET = "6LcM8DQUAAAAADxYIO2KLCPbBC-Dr4UE-tqZxY04";
+
+// Helper function to make API call to recatpcha and check response
+function verifyRecaptcha(key, callback) {
+        https.get("https://www.google.com/recaptcha/api/siteverify?secret=" + SECRET + "&response=" + key, function(res) {
+                var data = "";
+                res.on('data', function (chunk) {
+                        data += chunk.toString();
+                });
+                res.on('end', function() {
+                        try {
+                                var parsedData = JSON.parse(data);
+                                callback(parsedData.success);
+                        } catch (e) {
+                                callback(false);
+                        }
+                });
+        });
+}
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
